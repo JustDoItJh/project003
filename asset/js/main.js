fd0adfc245dcf25450de37e0ffab2c3060a5e06f
pjh <wlsgur0217@naver.com>
2022-07-19 23:35:28 +0900

    22.07.19

---
+++ asset/js/main.js
@@ -0,0 +1,37 @@
+$(function(){
+
+
+  
+    fetch('../asset/data/list.json')
+    .then((response)  => response.json())
+    .then((json) => {
+  
+      const airpods2nd = json.recent;
+      
+      let airpods2ndhtml = '';
+  
+      airpods2nd.forEach(element => {
+  
+        airpods2ndhtml +=  `<li class="info-item"><p class="text">${element.empty}</p></li>
+                            <li class="info-item"><p class="text">${element.empty}</p></li>
+                            <li class="info-item"><p class="text">${element.empty}</p></li>
+                            <li class="info-item">
+                                <img src="${element.imgSrc}" alt="">
+                                <p class="text">${element.infoCharge}</p>
+                            </li>
+                            <li class="info-item">
+                                <p class="text">
+                                    <span class="value">${element.time}</span>
+                                    <span class="sub-value">${element.infoTime} <sup><a href="#remark-16" class="remark">16</a></sup></span>
+                                </p>
+                            </li>`
+      });
+  
+      
+  
+      document.querySelector('.airpods-2nd .info-list').innerHTML = airpods2ndhtml;
+      
+  
+    })
+    
+})
