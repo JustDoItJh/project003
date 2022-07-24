a41c4beb37ac7cd2641096f4627b373cc29565b6
pjh <wlsgur0217@naver.com>
2022-07-24 15:53:39 +0900

    22.07.24

--- asset/js/common.js
+++ asset/js/common.js
@@ -1,5 +1,6 @@
 $(function(){
 
+    // Gnb Menu slide up&down
     lastScroll = 0;
 
     $(window).scroll(function(){
@@ -27,17 +28,33 @@ $(function(){
             $('.header').removeClass('dark')
         }
 
-
-
-
-        // if(currScroll > lastScroll) {
-        //     $('.header').addClass('hide')
-        // } else {
-        //     $('.header').removeClass('hide')
-        // }
-
-        // lastScroll = currScroll
-
     })
-    
+
+    // Header Lnb slide menu 
+    $('.btn-toggle').click(function(){
+        $('.menu-list').slideToggle();
+  
+        if($(this).hasClass('active')){
+            $(this).removeClass('active')
+            $('.btn-toggle::after').html('+');
+        } else {
+            $(this).addClass('active')
+            $('.btn-toggle::after').html('-');
+        }
+    });
+
+
+    // Footer 아코디언 메뉴
+    $('.nav-box').click(function(){
+        $('.nav-list').slideUp();
+
+        if($(this).children('.nav-list').is(':hidden')) {
+            $(this).children('.nav-list').slideDown();
+            $('.nav-box .title::after').animate({rotate:'45deg'}, 1000)
+        } else {
+            $(this).children('.nav-list').slideUp();
+            $('.nav-box .title::after').animate({rotate:'-45deg'}, 1000)
+        }
+    })
+
 });
